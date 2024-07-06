12ba2e5f2091f94fc8ba19832a92e6c70cc7fadd
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-06 20:48:07 +0330

    modifed getHumanChoice to accept full words.
    inputs are parsed case-insensitively.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // Returns a random choice between "rock", "paper", and "scissors"
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
@@ -21,22 +24,24 @@ function getComputerChoice() {
 // Gets and returns player choice. Reprompts the user if input was invalid.
 function getHumanChoice() {
     let input = prompt("Make a choice! [R]ock, [P]aper, [S]cissors: ");
+    input = input.toLowerCase();
+
     let humanChoice;
     switch (input) {
+        case "rock":
         case "r":
-        case "R":
             humanChoice = "rock";
             break;
+        case "paper":
         case "p":
-        case "P":
             humanChoice = "paper";
             break;
+        case "scissors":
         case "s":
-        case "S":
             humanChoice = "scissors";
             break
         default:
-            console.log("Invalid input. Type r for rock, p for paper and s for scissors.")
+            console.log("Invalid input. Try again!")
             humanChoice = getHumanChoice();
     }
     return humanChoice;
